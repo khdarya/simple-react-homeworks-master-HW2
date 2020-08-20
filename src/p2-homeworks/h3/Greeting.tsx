c8111920c759b3e4f44220506f439234dda20885
khdarya <darya.khakhlova@gmail.com>
2020-08-20 10:32:21 -0400

    hw5

---
+++ src/p2-homeworks/h3/Greeting.tsx
@@ -0,0 +1,34 @@
+import React, {ChangeEvent} from "react";
+import s from "./Greeting.module.css";
+
+type GreetingPropsType = {
+    name: string // need to fix any
+    setNameCallback: (e: ChangeEvent<HTMLInputElement>) => void // need to fix any
+    addUser: () => void // need to fix any
+    error: boolean // need to fix any
+    totalUsers: any // need to fix any
+}
+
+// презентационная компонента (для верстальщика)
+const Greeting: React.FC<GreetingPropsType> = (
+    {name, setNameCallback, addUser, error, totalUsers} // деструктуризация пропсов
+) => {
+    let inputClass = s.greetingInput; // need to fix with (?:)
+    if(error) {
+        inputClass += `${s.error}`
+    }
+
+    return (
+        <div>
+            <input
+                value={name}
+                onChange={setNameCallback}
+                className={inputClass}/>
+            <span>{error}</span>
+            <button onClick={addUser}>add</button>
+            <span>{totalUsers}</span>
+        </div>
+    );
+}
+
+export default Greeting;
